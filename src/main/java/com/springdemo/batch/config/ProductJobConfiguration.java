15809f4e7da7310cd996b7d7759bac8e00c0240f
garaiHaythem <garai.haythem@gmail.com>
2022-05-02 03:05:17 +0200

    add comment

--- src/main/java/com/springdemo/batch/config/ProductJobConfiguration.java
+++ src/main/java/com/springdemo/batch/config/ProductJobConfiguration.java
@@ -25,6 +25,8 @@ import org.springframework.core.io.ClassPathResource;
 
 import javax.persistence.EntityManagerFactory;
 
+
+// Spring Batch Configuration
 @Configuration
 @EnableBatchProcessing
 @Description("Product Job Configuration")
